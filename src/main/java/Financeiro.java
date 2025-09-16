be0177fd744875bc064b21c5b86a80c2fea530cf
Gustavo0606 <gustavovilelas22@gmail.com>
2025-09-15 21:09:29 -0300

    Update Financeiro.java
    Atualização Java financeiro

--- src/main/java/Financeiro.java
+++ src/main/java/Financeiro.java
@@ -1,38 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Financeiro {
-    private double precoBase;
-    private int totalIng;
-    private int totalInteira;
-    private int totalMeia;
-    private int totalPromo;
-    private int totalGratis;
-    private double totalArrecadado;
+    private List<Venda> vendas;
 
-    public Financeiro(double precoBase) {
-        this.precoBase = precoBase;
-}
-    public void registrarVenda(int quantidade, string tipoIngresso, double idadeCliente){
-        venda v = new Venda(quantidade, tipoIngresso, idadeCliente, precoBase)
-
-    totalIng += v.getQuantidade;
-    switch(v.getTipoingresso()){
-        case "inteira": totalInteira += v.getQuantidade();break;
-        case "meia": totalMeia += v.getQuantidade();break;
-        case "promocional": totalPromo += v.getQuantidade();break;
-        case "gratuito": totalGratis += v.getQuantidade();break;
-}
-    totalArrecadado += v.getValorTotal;
-}
-    public void exibirRelatorioFinal(){
-        System.out.println("\n relatorio final");
-        System.out.println("Total de ingressos vendidos "+ totalIng);
-        System.out.println("inteira: " + totalInteira);
-        System.out.println("meia:" + totalMeia);
-        System.out.println("promoção "+ totalPromo)
-        System.out.println("gratuito"+ totalGratis)
-        System.out.printf("Total arrecadado: R$ %.2f%n", totalArrecadado);
-}
-    public double getPrecoBase() {
-            return precoBase;
+    public Financeiro() {
+        this.vendas = new ArrayList<>();
     }
+
+    public void registrarVenda(Venda v) {
+        vendas.add(v);
+    }
+
+
+ public void exibirRelatorioFinal() {
+        int totalIngressos = 0;
+        int totalInteira = 0;
+        int totalMeia = 0;
+        int totalPromocional = 0;
+        int totalGratis = 0;
+        double totalArrecadado = 0.0;
+
+        for (Venda v : vendas) {
+            List<String> tipos = v.getTipos();
+            totalIngressos += tipos.size();
+
+            for (String tipo : tipos) {
+                switch (tipo) {
+                    case "inteira" -> totalInteira+= 1;
+                    case "meia" -> totalMeia+= 1;
+                    case "promocional" -> totalPromocional+= 1;
+                    case "gratuito" -> totalGratis+= 1;
+                }
 }
+totalArrecadado += v.getValorTotal;
+}
+
+    System.out.println("\n relatorio final");
+    System.out.println("Total de ingressos vendidos "+ totalIngressos);
+    System.out.println("inteira: " + totalInteira);
+    System.out.println("meia:" + totalMeia);
+    System.out.println("promoção "+ totalPromocional)
+    System.out.println("gratuito"+ totalGratis)
+    System.out.printf("Total arrecadado: R$ %.2f%n", totalArrecadado);
+}
+    }
+
 
